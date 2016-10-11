ea73b591ffc83eccb8495abd38bc9e77b85a6017
Christian <solero93@gmail.com>
2016-10-11 16:47:08 +0200

    Internal/create base classes (#52)
    Created Base classes and updated diagrams

---
+++ Assets/Scripts/Model/Tower.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour, Upgradeable
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	// To upgrade when there are enough coins
+	public void Upgrade ()
+	{
+
+	}
+}
---
+++ Assets/Scripts/Model/Upgradeable.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+interface Upgradeable
+{
+	void Upgrade ();
+}
