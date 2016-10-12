6ca7449d51d76c09ed21668c51428d298ae0ba93
Solero93 <solero93@gmail.com>
2016-10-12 22:36:34 +0200

    Added Reparaible interface

--- Assets/Scripts/Model/Wall.cs
+++ Assets/Scripts/Model/Wall.cs
@@ -1,23 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class Wall : MonoBehaviour, Upgradeable
+public class Wall : MonoBehaviour, Upgradeable, Reparaible
 {
-	// Use this for initialization
-	void Start ()
-	{
+    // Use this for initialization
+    void Start ()
+    {
 
-	}
+    }
 
-	// Update is called once per frame
-	void Update ()
-	{
+    // Update is called once per frame
+    void Update ()
+    {
 
-	}
+    }
 
-	// To upgrade when there are enough coins
-	public void Upgrade ()
-	{
+    // To upgrade when there are enough coins
+    public void Upgrade ()
+    {
 
-	}
+    }
+
+    public void Repair ()
+    {
+
+    }
 }
