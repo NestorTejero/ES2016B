2424f26f87c38388985b6e0c7877616544273652
Karen Samsó <ksamsomu7@alumnes.ub.edu>
2016-10-14 01:16:17 +0200

    Feature/ Tower fire logic (#57)
    * First commit
    
    * Tower fire logic implemented & scene changes
    
    Tests done on towers with Weapon:
    - range = 3
    - power = 1
    - cooldown = 3
    
    And enemies with Unit:
    - health = 10
    
    * Empty Unit
    
    * After review changes
    
    - Weapon objects on towers (Scene)
    - Health in Attacker deleted
    - rig.cs file deleted
    - Weapon assignment to each tower (and their script)
    - Testing attributes and movement command in Unit

--- Assets/Scripts/Model/Tower.cs
+++ Assets/Scripts/Model/Tower.cs
@@ -1,19 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Tower : MonoBehaviour, Upgradeable, CanAttack
 {
+	public GameObject tower;
+	public CapsuleCollider towerRange;
+	public Unit enemy;
+	public Weapon weapon;
+	public GameObject enemy_object;
+	public bool enemy_in;
+	public List<Unit> enemies;
+
 
 	// Use this for initialization
 	void Start ()
 	{
-	
+		// gameObject attached to this script
+		tower = this.gameObject;
+
+		// Collider of the tower
+		towerRange = tower.GetComponent<CapsuleCollider> ();
+
+		// Assignes the radius of the collider as the weapon attack range
+		towerRange.radius = weapon.range;
+
+		// Fires every "cooldown of the weapon" seconds
+		InvokeRepeating("Attack", 0.0f, weapon.cooldown);
+
+		// No enemy inside the range at the beginning
+		enemy_in = false;
+
+		// List of enemies when more than one enters the range
+		enemies = new List<Unit> ();
+
+		Debug.Log("TOWER");
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		
 	}
 
 	// To upgrade when there are enough coins
@@ -25,6 +52,41 @@ public class Tower : MonoBehaviour, Upgradeable, CanAttack
 	// To attack enemies
 	public void Attack ()
 	{
+		// Cheks if there is an enemy in the range
+		if(enemy_in && enemy != null){
+			if (enemy.health > 1) {
+				enemy.health = enemy.health - weapon.power;
+				Debug.Log("Enemy's health: "+enemy.health);
+			} else {
+				enemy_object = enemy.gameObject;
+				Destroy (enemy_object);
+				Debug.Log("Enemy dead");
 
+				if (enemies.Count > 0) {
+					Debug.Log ("Another enemy");
+					enemy = enemies [0];
+					enemies.Remove (enemy);
+				} else {
+					enemy_in = false;
+				}
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+		Debug.Log ("Collision");
+		if (!enemy_in) {
+			enemy_in = true;
+			Debug.Log ("Enemy In");
+
+			// Gets the script of the Collider that entered the radius of the tower
+			enemy = col.gameObject.GetComponent<Unit> ();
+
+		} else {
+			// Adds enemy to attack to the queue
+			enemies.Add (col.gameObject.GetComponent<Unit> ());
+			Debug.Log ("Enemy in queue");
+		}
+			
 	}
 }
--- Assets/Scripts/Model/Weapon.cs
+++ Assets/Scripts/Model/Weapon.cs
@@ -3,10 +3,21 @@ using System.Collections;
 
 public class Weapon : MonoBehaviour
 {
+	public int range;
+	public int power;
+	public int cooldown;
+
+	//criticalMultiplier;
+	//criticalChance;
+	
 	// Use this for initialization
 	void Start ()
 	{
+		range = 3;
+		power = 1;
+		cooldown = 3;
 
+		Debug.Log ("WEAPON");
 	}
 
 	// Update is called once per frame
