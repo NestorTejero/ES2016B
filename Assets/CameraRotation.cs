f31bc0810545c25bf10971eb3f087ab763a28cb9
Joaquim Gifre <quimix.gr93@gmail.com>
2016-10-14 10:08:47 +0200

    Il·luminació amb cicle dia/nit y moviment de camara limitat i funcionant amb les fletxes de direcció

---
+++ Assets/CameraRotation.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+	public float moveSpeedV = 10f;
+	public float moveSpeedH = 30f;
+
+	public Transform ubCenter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Vector3 position = transform.position;
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			position = position + (Vector3.up * moveSpeedV * Time.deltaTime);
+			if (position.y <= 40)
+				transform.Translate (Vector3.up * moveSpeedV * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			position = position + (Vector3.up * moveSpeedV * Time.deltaTime);
+			if (position.y >= 20)
+				transform.Translate (-Vector3.up * moveSpeedV * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow))
+			transform.Translate (Vector3.left*moveSpeedV*Time.deltaTime);
+		if (Input.GetKey (KeyCode.RightArrow))
+			transform.Translate (-Vector3.left*moveSpeedV*Time.deltaTime);
+
+		transform.LookAt (ubCenter.transform.position);
+	}
+}
---
+++ Assets/DayLightCycle.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayLightCycle : MonoBehaviour {
+
+	public float dayLength;
+	public float nightLength;
+	public float time;
+	public float hour;
+
+	public Transform ubCenter;
+	public Light sun;
+	public Light moon;
+
+	public ParticleSystem stars;
+	private bool starsOn;
+
+	// Use this for initialization
+	void Start () {
+		dayLength = 2.0f;
+		nightLength = 1.0f;
+		time = 2.0f;
+		hour = 0.0f;
+		starsOn = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		sun.transform.RotateAround (ubCenter.transform.position,Vector3.right,Time.deltaTime/time);
+		sun.transform.LookAt (ubCenter);
+
+		moon.transform.RotateAround (ubCenter.transform.position,Vector3.right,Time.deltaTime/time);
+		moon.transform.LookAt (ubCenter);
+
+		stars.transform.Rotate (Vector3.right*(Time.deltaTime/4));
+
+		hour += Time.deltaTime / time;
+
+		if (hour >= 360) {
+			hour = 0.0f;
+		}
+
+		if (hour >= 180) {
+			//Starting night time
+			time = nightLength;
+			sun.intensity -= Time.deltaTime;
+			moon.intensity += Time.deltaTime;
+			if (starsOn == false) {
+				stars.Play ();
+				starsOn = true;
+			}
+
+		} else {
+			//Starting day time
+			time = dayLength;
+			sun.intensity += Time.deltaTime;
+			moon.intensity -= Time.deltaTime;
+			if (starsOn == true) {
+				stars.Stop ();
+				stars.Clear ();
+				starsOn = false;
+			}
+		}
+
+		if (sun.intensity >= 0.9f) {
+			sun.intensity = 0.9f;
+		}
+		if (sun.intensity <= 0.2f) {
+			sun.intensity = 0.2f;
+		}
+
+		if (moon.intensity >= 0.5f) {
+			moon.intensity = 0.5f;
+		}
+		if (moon.intensity <= 0.1f) {
+			moon.intensity = 0.1f;
+		}
+
+	}
+}
