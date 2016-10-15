e3a810c1aabe9d8f389fbab96a55bc2bf1d13323
Joaquim Gifre <quimix.gr93@gmail.com>
2016-10-15 13:31:45 +0200

    Afegits una mica de comentaris als scripts per fer-los més entendibles

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class CameraRotation : MonoBehaviour {
 
+	//This are the speeds the camera will turn arround
 	public float moveSpeedV = 10f;
 	public float moveSpeedH = 30f;
 
+	//This is the point the camera always will look at
 	public Transform ubCenter;
 
 	// Use this for initialization
@@ -13,26 +15,34 @@ public class CameraRotation : MonoBehaviour {
 	
 	}
 	
-	// Update is called once per frame
+	// Using FixedUpdate because we are messing with the camera
 	void FixedUpdate () {
 
+		//Creating a vectorposition to limit the height max and min of the camera
 		Vector3 position = transform.position;
 
+		//Using the keys to control the camera
 		if (Input.GetKey (KeyCode.UpArrow)) {
+			//noticing which will be the future height position to compare it later
 			position = position + (Vector3.up * moveSpeedV * Time.deltaTime);
+			//We just want the camera to move if our next height is below the max
 			if (position.y <= 40)
 				transform.Translate (Vector3.up * moveSpeedV * Time.deltaTime);
 		}
 		if (Input.GetKey (KeyCode.DownArrow)) {
+			//noticing which will be the future height position to compare it later
 			position = position + (Vector3.up * moveSpeedV * Time.deltaTime);
+			//We just want the camera to move if our next height is over the min
 			if (position.y >= 20)
 				transform.Translate (-Vector3.up * moveSpeedV * Time.deltaTime);
 		}
+
 		if (Input.GetKey (KeyCode.LeftArrow))
 			transform.Translate (Vector3.left*moveSpeedV*Time.deltaTime);
 		if (Input.GetKey (KeyCode.RightArrow))
 			transform.Translate (-Vector3.left*moveSpeedV*Time.deltaTime);
 
+		//To make sure the camera points the right way
 		transform.LookAt (ubCenter.transform.position);
 	}
 }
--- Assets/Scripts/DayLightCycle.cs
+++ Assets/Scripts/DayLightCycle.cs
@@ -3,12 +3,13 @@ using System.Collections;
 
 public class DayLightCycle : MonoBehaviour {
 
+	//This variables set the length of the day and night
 	public float dayLength;
 	public float nightLength;
-	public float time;
-	public float hour;
+	public float time;	//This one will be equal to dayLength or NightLength depending on if it's night or day
+	public float hour;	//This will give the hour assuming the clock has the 24h in the 360 degrees
 
-	public Transform ubCenter;
+	public Transform ubCenter; //This is the spot the light will be focusing while it's turning
 	public Light sun;
 	public Light moon;
 
@@ -24,22 +25,28 @@ public class DayLightCycle : MonoBehaviour {
 		starsOn = true;
 	}
 	
-	// Update is called once per frame
+	// Using fixedUpdate to smooth camera issues
 	void FixedUpdate () {
+
+		//This calculates the rotation of the sun and moon.
 		sun.transform.RotateAround (ubCenter.transform.position,Vector3.right,Time.deltaTime/time);
 		sun.transform.LookAt (ubCenter);
 
 		moon.transform.RotateAround (ubCenter.transform.position,Vector3.right,Time.deltaTime/time);
 		moon.transform.LookAt (ubCenter);
 
+		//the stars will rotate as well to make a nicer efect
 		stars.transform.Rotate (Vector3.right*(Time.deltaTime/4));
 
 		hour += Time.deltaTime / time;
 
+		//Checking if it's a new day
 		if (hour >= 360) {
 			hour = 0.0f;
 		}
 
+		//Assuming the day it's splited in half with the night, we change lights intensities
+		//during all the process and play/stop the stars particleSystem
 		if (hour >= 180) {
 			//Starting night time
 			time = nightLength;
@@ -62,6 +69,7 @@ public class DayLightCycle : MonoBehaviour {
 			}
 		}
 
+		//finally, including limits on the max and min intensity of the lights
 		if (sun.intensity >= 0.9f) {
 			sun.intensity = 0.9f;
 		}
