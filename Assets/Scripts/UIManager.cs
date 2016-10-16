a2e83e267884dc87e764420eba181fdb7de13f06
JMeyerr <freeclimber46@gmail.com>
2016-10-16 20:03:15 +0200

    basic game menu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+	
+	GameObject[] pauseObjects;
+	AudioSource audio;
+	
+	public Toggle audioToggle;
+
+	
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();
+		Time.timeScale = 1;
+		pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+		hidePaused();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//uses the p button to pause and unpause the game
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			if(Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				showPaused();
+			} else if (Time.timeScale == 0){
+				Debug.Log ("high");
+				Time.timeScale = 1;
+				hidePaused();
+			}
+		}
+	}
+
+
+	//Reloads the Level
+	public void Reload(){
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	//controls the pausing of the scene
+	public void pauseControl(){
+			if(Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				showPaused();
+			} else if (Time.timeScale == 0){
+				Time.timeScale = 1;
+				hidePaused();
+			}
+	}
+
+	//shows objects with ShowOnPause tag
+	public void showPaused(){
+		foreach(GameObject g in pauseObjects){
+			g.SetActive(true);
+		}
+	}
+
+	//hides objects with ShowOnPause tag
+	public void hidePaused(){
+		foreach(GameObject g in pauseObjects){
+			g.SetActive(false);
+		}
+	}
+
+	//loads inputted level
+	public void LoadLevel(string level){
+		Application.LoadLevel(level);
+	}
+	
+	//mutes and unmutes the sound
+	public void setAudio(){
+		if(audioToggle.isOn){
+			audio.mute = false;
+			Debug.Log("Audio on.");
+		}
+		else{
+			audio.mute = true;
+			Debug.Log("Audio off.");
+		}		
+	}
+}
