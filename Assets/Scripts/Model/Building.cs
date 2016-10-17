779bbcf1250e479a78582983c3d9b2f07facc3ca
JoanBenito <jbenitma7@alumnes.ub.edu>
2016-10-17 22:33:18 +0200

    jaja saludos

--- Assets/Scripts/Model/Building.cs
+++ Assets/Scripts/Model/Building.cs
@@ -7,25 +7,19 @@ public class Building : MonoBehaviour, CanUpgrade, CanRepair, CanReceiveDamage
 	public float health;
 	public float upgradeHealthFactor;
 	public float repairHealthQuantity;
-    private float timeToWin;
 
 	// Use this for initialization
 	void Start ()
 	{
 		this.health = this.maxHealth;
-        this.timeToWin = 10.0f;
 		Debug.Log ("BUILDING CREATED with HP: " + this.maxHealth);
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-        if (this.timeToWin <= 0.0f)
-        {
-            Application.LoadLevel("MainMenu");
-        }
-        this.timeToWin -= Time.deltaTime * 1;
-    }
+
+	}
 
 	// To upgrade when there are enough coins
 	public void Upgrade ()
@@ -48,7 +42,7 @@ public class Building : MonoBehaviour, CanUpgrade, CanRepair, CanReceiveDamage
 	{
 		this.health -= wep.power;
 		if (this.health <= 0.0) {
-            Application.LoadLevel("MainMenu");
+			// TODO put winning / losing condition logic here
 		}
 		Debug.Log ("BUILDING DAMAGED by HP: " + wep.power);
 	}
