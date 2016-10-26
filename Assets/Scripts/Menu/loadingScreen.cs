063baeddd3953971d34bb09c1c256a7487f851b2
ire925 <ire925@gmail.com>
2016-10-26 17:23:04 +0200

    loading Scene

---
+++ Assets/Scripts/Menu/loadingScreen.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class loadingScreen : MonoBehaviour {
+	public float sayi;
+	public Text sayiyazi,ipucuyari;
+	public Image bar;
+
+	void Start () {
+
+		//loadingBar.fillAmount = 0;
+
+	}
+
+
+	void Update () {
+
+		if (sayi == 100) {
+			SceneManager.LoadScene(1); 
+		}
+
+
+		if (sayi < 100) {
+
+			sayi += Time.deltaTime * 7;
+
+		}
+
+		if (sayi >= 100) {
+			sayi = 100;
+		}
+
+		sayiyazi.text=""+(int)sayi+"%";
+		bar.transform.localScale = new Vector3 (sayi/100, 1, 1);
+
+
+	}
+
+
+}
+	
