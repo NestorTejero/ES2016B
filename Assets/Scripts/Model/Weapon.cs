9b1165b265e82f6bd5c20bced22076fff9846e3a
Karen Samsó <ksamsomu7@alumnes.ub.edu>
2016-10-29 14:49:44 +0200

    Attack and ReceiveDamage logic (#130)
    * Reorganizing responsabilities
    
    - Attack in Weapon logic
    - Projectile creation
    - ReceiveDamage in Unit
    - Getters and setters
    - ...
    
    * Projectile creation fixed
    
    * Target assignment (to fix)
    
    * First version
    
    - Possible organization of responsabilities (v1)
    - Target management on Weapon
    - Weapons prefabs removed of TestScene
    - Changes in Tower prefabs (added Weapon script)
    - Some values changed in prefabs
    
    * Attack / ReceiveDamage logic and prefabs changes
    
    - Freshman_student (enemy) prefab edited
    - Building prefab edited
    Both changes done due to a correct collision (for the attack logic)
    - Weapon prefab removed (not necessary)
    - TestScene changed with the new prefabs
    - Attack/Receive Damage logic implemented as follows:
    *Tower and Unit detect the collisions, therefore, they pass the target
    that entered their range to their Weapon (that is in charge of attacking
    a queue of targets).
    *Building and Unit receive the damage and the game ends or they destroy
    themselves, respectively, if their health reaches 0.
    
    * PR changes done
    
    - Passing down the damage of the weapon to their projectiles.
    - Unit and Building damage received by a projectile.
    - Extra: check if some target leaves the attack zone (range) to remove
    it from the target list.
    - Note: Projectile has to hit (collide with) a target to damage them.
    For now, there is no physical object of Projectile (it simply damages
    the target when calling the shoot() method).
    - Tested: multiple targets in the same tower range.

--- Assets/Scripts/Model/Weapon.cs
+++ Assets/Scripts/Model/Weapon.cs
@@ -1,29 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Weapon : MonoBehaviour, CanUpgrade
 {
     public float baseDamage;
-    public float baseRadius;
     public float baseRange;
     public float baseCooldown;
+	public float upgradeFactor;
+	public AudioClip shootSound;
 
-    // TODO change all these to private
-    public float currentDamage; 
-    public float currentRadius;
-    public float currentRange;
-    public float currentCooldown;
-    //
-
-    public float upgradeFactor;
+    private float currentDamage;
+    private float currentRange;
+    private float currentCooldown;
+	private List<CanReceiveDamage> targets;
+	private AudioSource source;
 
 	// Use this for initialization
 	void Start ()
 	{
 	    this.currentDamage = this.baseDamage;
-	    this.currentRadius = this.baseRadius;
 	    this.currentRange = this.baseRange;
 	    this.currentCooldown = this.baseCooldown;
+
+		// Collider of the tower attached to this script
+		this.gameObject.GetComponent<CapsuleCollider> ().radius = this.currentRange;
+
+		// List of targets assigned to the weapon
+		this.targets = new List<CanReceiveDamage>();
+
+		// Call Attack every 'cooldown' seconds
+		InvokeRepeating("Attack", 0.0f, this.currentCooldown);
+
+		Debug.Log ("WEAPON CREATED");
 	}
 
 	// Update is called once per frame
@@ -32,13 +41,57 @@ public class Weapon : MonoBehaviour, CanUpgrade
 
 	}
 
+	// Upgrade weapon features
 	public void Upgrade ()
 	{
 		this.currentDamage *= upgradeFactor;
 	}
+		
+	//USeed for sounds
+	void Awake()
+	{
+		this.source = GetComponent<AudioSource>();
+	}
 
-    /*public float getDamage()
+	// Get weapon's current damage
+    public float getCurrentDamage()
     {
         return this.currentDamage;
-    }*/
+    }
+
+	// Add target to list
+	public void addTarget(CanReceiveDamage target){
+		this.targets.Add (target);
+		Debug.Log ("Targets to attack :" + targets.Count);
+	}
+
+	// Remove target from list
+	public void removeTarget(CanReceiveDamage target){
+		this.targets.Remove (target);
+	}
+
+	// Called to attack a target
+	public void Attack()
+	{
+		// Checks if there is a target in the range
+		if (this.targets.Count > 0) {
+			Awake ();
+
+			// Get target to attack
+			CanReceiveDamage target = targets [0];
+
+			// Create the projectile that will go towards the target
+			Projectile projectile = this.gameObject.AddComponent<Projectile>();
+			projectile.create(1.0f, target, this.currentDamage);
+
+			// Shoot the projectile
+			bool dead = projectile.shoot ();
+			if (dead) {
+				this.targets.Remove (target);
+			}
+			float vol = Random.Range (.5f, 1.0f);
+			this.source.PlayOneShot (this.shootSound, vol);
+		}
+	}
 }
+
