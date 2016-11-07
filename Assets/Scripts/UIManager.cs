b146c7c400a9c0807d5dfff982cda98b296ba1de
JMeyerr <freeclimber46@gmail.com>
2016-11-08 00:26:21 +0100

    rehecho menu y tooltips

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,25 +5,31 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour {
 
 	GameObject[] pauseObjects;
-	AudioSource audio;
-
-	public Toggle audioToggle;
 	
 	// Text field that will show the tooltip:
 	public Text TooltipText;
 
 	//we need this boolean to know if game is paused
 	bool gamePaused;
+	
+	// Control of volume:
+	public AudioListener audioListener;
+	public Slider volumeSlider;
 
 
 	// Use this for initialization
 	void Start () {
-		audio = GetComponent<AudioSource>();
 		Time.timeScale = 1;
 		pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
 		hidePaused();
 		gamePaused = false;
 		TooltipText = GameObject.Find("TooltipText").GetComponent<Text>();
+		
+		GameObject temp = GameObject.Find("EffectsSlider");
+		volumeSlider = temp.GetComponent<Slider>();
+		volumeSlider.onValueChanged.AddListener (delegate {ValueChangeCheck ();});
+		GameObject tempo = GameObject.Find("Main Camera(RTS)");
+		audioListener = tempo.GetComponent<AudioListener>();
 	}
 
 	// Update is called once per frame
@@ -45,6 +51,12 @@ public class UIManager : MonoBehaviour {
 			}
 		}
 
+	}
+	
+	// Changes the effects sound
+	public void ValueChangeCheck()
+	{
+		//audioListener.volume = volumeSlider.value;
 	}
 
 
@@ -80,15 +92,4 @@ public class UIManager : MonoBehaviour {
 		Application.LoadLevel(level);
 	}
 
-	//mutes and unmutes the sound
-	public void setAudio(){
-		if(audioToggle.isOn){
-			audio.mute = false;
-			Debug.Log("Audio on.");
-		}
-		else{
-			audio.mute = true;
-			Debug.Log("Audio off.");
-		}
-	}
 }
