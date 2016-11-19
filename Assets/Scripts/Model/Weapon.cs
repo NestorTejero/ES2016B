2a76d7614bc3019b1c7eca80721148b3940361ec
vcampoma7 <vcampoma7@alumnes.ub.edu>
2016-11-19 12:51:35 +0100

    random death sound

--- Assets/Scripts/Model/Weapon.cs
+++ Assets/Scripts/Model/Weapon.cs
@@ -7,100 +7,117 @@ public class Weapon : MonoBehaviour
     public float baseDamage;
     public float baseRange;
     public float baseCooldown;
-	public float upgradeFactor;
+    public float upgradeFactor;
     public AudioClip shootSound, deathSound1, deathSound2, deathSound3;
     public AudioSource source_shoot, source_death;
+    public AudioClip[] death;
 
     private float currentDamage;
     private float currentRange;
     private float currentCooldown;
-	private List<CanReceiveDamage> targets;
-    private AudioSource source;
-	private Projectile projectile;
+    private List<CanReceiveDamage> targets;
+    private Projectile projectile;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    this.currentDamage = this.baseDamage;
-	    this.currentRange = this.baseRange;
-	    this.currentCooldown = this.baseCooldown;
+    // Use this for initialization
+    void Start()
+    {
+        this.currentDamage = this.baseDamage;
+        this.currentRange = this.baseRange;
+        this.currentCooldown = this.baseCooldown;
 
-		// Collider of the tower attached to this script
-		this.gameObject.GetComponentInChildren<CapsuleCollider>().radius = this.currentRange;
+        // Collider of the tower attached to this script
+        this.gameObject.GetComponentInChildren<CapsuleCollider>().radius = this.currentRange;
 
-		// List of targets assigned to the weapon
-		this.targets = new List<CanReceiveDamage>();
+        // List of targets assigned to the weapon
+        this.targets = new List<CanReceiveDamage>();
 
-		// Create the projectile that will go towards the target
-		this.projectile = this.gameObject.GetComponentInChildren<Projectile>();
+        // Create the projectile that will go towards the target
+        this.projectile = this.gameObject.GetComponentInChildren<Projectile>();
 
-		// Call Attack every 'cooldown' seconds
-		InvokeRepeating("Attack", 0.0f, this.currentCooldown);
+        // Call Attack every 'cooldown' seconds
+        InvokeRepeating("Attack", 0.0f, this.currentCooldown);
 
         // Set sounds
-        this.source = GetComponent<AudioSource>();
+        death = new AudioClip[]
+        {
+            (AudioClip)Resources.Load("Sound/Effects/Death 1"),
+            (AudioClip)Resources.Load("Sound/Effects/Death 2"),
+            (AudioClip)Resources.Load("Sound/Effects/Death 3")
+        };
 
-        Debug.Log ("WEAPON CREATED");
-	}
+        Debug.Log("WEAPON CREATED");
+    }
 
     // Upgrade weapon features
-    public void Upgrade ()
-	{
-		this.currentDamage *= upgradeFactor;
-	}
+    public void Upgrade()
+    {
+        this.currentDamage *= upgradeFactor;
+    }
 
-	// Get weapon's current damage
+    // Get weapon's current damage
     public float getCurrentDamage()
     {
         return this.currentDamage;
     }
 
-	// Add target to list
-	public void addTarget(CanReceiveDamage target){
-		this.targets.Add (target);
-		Debug.Log (this.gameObject.name + "-> Targets to attack :" + targets.Count);
-	}
+    // Add target to list
+    public void addTarget(CanReceiveDamage target)
+    {
+        this.targets.Add(target);
+        Debug.Log(this.gameObject.name + "-> Targets to attack :" + targets.Count);
+    }
 
-	// Remove target from list
-	public void removeTarget(CanReceiveDamage target){
-		this.targets.Remove (target);
+    // Remove target from list
+    public void removeTarget(CanReceiveDamage target)
+    {
+        this.targets.Remove(target);
         // Play death sound
         if (!this.source_death.isPlaying)
-            this.source_death.PlayOneShot(this.deathSound1);
-        Debug.Log (this.gameObject.name + "-> Targets to attack :" + targets.Count);
-	}
+        {
+            //audio.PlayOneShot(list[number], 0.5f);
+            source_death.PlayOneShot(death[Random.Range(0, death.Length)], 0.5f);
+        }
 
-	// Get the available target to attack from the targets list
-	public CanReceiveDamage getAvailableTarget(){
+        Debug.Log(this.gameObject.name + "-> Targets to attack :" + targets.Count);
+    }
 
-		// Checks if there is a target in the range
-		while (this.targets.Count > 0) {
+    // Get the available target to attack from the targets list
+    public CanReceiveDamage getAvailableTarget()
+    {
 
-			// Get target to attack
-			CanReceiveDamage target = this.targets [0];
+        // Checks if there is a target in the range
+        while (this.targets.Count > 0)
+        {
 
-			// Check if target is already dead
-			if (target.getCurrentHealth() <= 0.0f) {
-				Debug.Log (this.gameObject.name + ": TARGET ALREADY DEAD");
-				this.removeTarget (target);
-			} else {
-				Debug.Log (this.gameObject.name + ": TARGET AVAILABLE TO SHOOT");
-				return target;
-			}
-		}
-		Debug.Log (this.gameObject.name + ": NO TARGETS ON QUEUE");
-		return null;
-	}
+            // Get target to attack
+            CanReceiveDamage target = this.targets[0];
 
-	// Called to attack a target
-	public void Attack()
-	{
-		CanReceiveDamage target = getAvailableTarget();
-		if (target != null) {
-			
-			// Set the projectile properties and shoot
-			projectile.Properties (1.0f, target, this.currentDamage);
-			projectile.Shoot ();
+            // Check if target is already dead
+            if (target.getCurrentHealth() <= 0.0f)
+            {
+                Debug.Log(this.gameObject.name + ": TARGET ALREADY DEAD");
+                this.removeTarget(target);
+            }
+            else
+            {
+                Debug.Log(this.gameObject.name + ": TARGET AVAILABLE TO SHOOT");
+                return target;
+            }
+        }
+        Debug.Log(this.gameObject.name + ": NO TARGETS ON QUEUE");
+        return null;
+    }
+
+    // Called to attack a target
+    public void Attack()
+    {
+        CanReceiveDamage target = getAvailableTarget();
+        if (target != null)
+        {
+
+            // Set the projectile properties and shoot
+            projectile.Properties(1.0f, target, this.currentDamage);
+            projectile.Shoot();
 
             // Play shoot sound
             if (!this.source_shoot.isPlaying)
@@ -108,6 +125,6 @@ public class Weapon : MonoBehaviour
                 this.source_shoot.PlayOneShot(this.shootSound);
             }
         }
-	}
+    }
 }
 
