abd4a9279a929fb7e4911bc8d126e5a8c11e8a20
ire925 <ire925@gmail.com>
2016-11-19 02:13:33 +0100

    options of MainMenu

--- Assets/Scripts/Menu/SubMenu.cs
+++ Assets/Scripts/Menu/SubMenu.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class SubMenu : MonoBehaviour {
 
@@ -10,6 +12,20 @@ public class SubMenu : MonoBehaviour {
 	public bool isReturnButton = false;
     public AudioSource audioSource;
 
+	// Control of volume:
+	public Slider volumeSlider = null;
+	public Slider musicSlider = null;
+
+	public Image musicIcon;
+	public Image effectsIcon;
+
+	public Sprite soundFull;
+	public Sprite soundMedium;
+	public Sprite soundLow;
+	public Sprite soundMuted;
+
+	private float lastMVolume; // Music volume
+	private float lastEVolume; // Effects volume
 
     // Use this for initialization
     void Start () {
@@ -17,6 +33,11 @@ public class SubMenu : MonoBehaviour {
 		Cursor.visible = true;
 		//The cursor will automatically be hidden, centered on view and made to never leave the view.
 		Screen.lockCursor = false;
+		// Sliders of Sound
+		volumeSlider.onValueChanged.AddListener (delegate {effectsChangeCheck ();});
+		musicSlider.onValueChanged.AddListener (delegate {musicChangeCheck ();});
+		lastMVolume = 1;
+		lastEVolume = 1;
 	}
 
 	public void OnMouseEnter(){
@@ -52,4 +73,76 @@ public class SubMenu : MonoBehaviour {
         yield return new WaitForSeconds(audioSource.clip.length);
         SceneManager.LoadScene(scene); //Load the game (next scene)  
     }
+
+
+	// Changes the effects sound
+	public void effectsChangeCheck(){
+		AudioListener.volume = volumeSlider.normalizedValue;
+		if (volumeSlider.value == 0){
+			effectsIcon.sprite = soundMuted;
+		}
+		else if(volumeSlider.normalizedValue < 0.5f){
+			effectsIcon.sprite = soundLow;
+		}
+		else if(volumeSlider.normalizedValue < 1.0f){
+			effectsIcon.sprite = soundMedium;
+		}
+		else{
+			effectsIcon.sprite = soundFull;
+		}
+	}
+
+	// Changes the music sound
+	public void musicChangeCheck(){
+		AudioListener.volume = volumeSlider.normalizedValue;
+		if (musicSlider.value == 0){
+			musicIcon.sprite = soundMuted;
+		}
+		else if(musicSlider.normalizedValue < 0.5f){
+			musicIcon.sprite = soundLow;
+		}
+		else if(musicSlider.normalizedValue < 1.0f){
+			musicIcon.sprite = soundMedium;
+		}
+		else{
+			musicIcon.sprite = soundFull;
+		}
+	}
+
+
+
+	public void effectsIconClick(){
+		if (AudioListener.volume == 0){
+			if (lastEVolume == 0){
+				lastEVolume = 1;
+			}
+			AudioListener.volume = lastEVolume;
+			volumeSlider.value = lastEVolume;
+			effectsChangeCheck();
+		}
+		else{
+			float aux = AudioListener.volume;
+			AudioListener.volume = 0;
+			volumeSlider.value = 0;
+			effectsChangeCheck();
+			lastEVolume = aux;
+
+		}
+	}
+
+	public void musicIconClick(){
+		if (musicSlider.value == 0){
+			if (lastMVolume == 0){
+				lastMVolume = 1;
+			}
+			musicSlider.value = lastMVolume;
+			musicChangeCheck();
+		}
+		else{
+			float aux = musicSlider.value;
+			musicSlider.value = 0;
+			musicChangeCheck();
+			lastMVolume = aux;
+		}
+	}
 }
