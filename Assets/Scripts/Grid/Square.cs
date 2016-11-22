c482d54d358cd440588795c037c5a4a7dc4002eb
Joaquim Gifre <quimix.gr93@gmail.com>
2016-11-22 22:11:00 +0100

    new branch in order to merge grid with dev

---
+++ Assets/Scripts/Grid/Square.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Square{
+	//variables that each cell will have.
+	public bool valid;
+	public Vector3 worldPosition;
+	public GameObject cell;
+
+	//constructor
+	public Square(bool _valid, Vector3 _worldPos, GameObject _cell){
+		valid = _valid;
+		worldPosition = _worldPos;
+		cell = _cell;
+	}
+}
