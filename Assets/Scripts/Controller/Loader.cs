8a0016da036241fd398ef441f0f690de5f97bdf5
niko227 <nk_bcn227@hotmail.com>
2016-11-22 17:33:03 +0100

    Aimenu working correct

--- Assets/Scripts/Controller/Loader.cs
+++ Assets/Scripts/Controller/Loader.cs
@@ -8,19 +8,28 @@ public class Loader : MonoBehaviour
 {
     public GameObject gameController;
 	public GameObject MediumAI;
+	public GameObject EasyAI;
+	public GameObject HardAI;
 
     private void Awake()
     {
         //Check if a GameManager has already been assigned to static variable GameController.instance or if it's still null
-        if (GameController.instance == null)
+		if (GameController.instance == null) 
+			//Instantiate GameController prefab
+			Instantiate (gameController);
 
-            //Instantiate GameController prefab
-            Instantiate(gameController);
-			
-			//Instantiate(MediumAI);
-			//MediumAI = (GameObject)Instantiate (Resources.Load ("Prefabs/MediumAI"));
-		 //MediumAI = Instantiate(Resources.Load("Prefabs/Players/EasyAI")) as GameObject;
-    }
+	}
+
+	private void Start(){
+		if (PersistentValues.difficulty == 1) {
+			Instantiate (EasyAI);
+		} else if (PersistentValues.difficulty == 2) {
+			Instantiate (EasyAI);
+		} else if (PersistentValues.difficulty == 3) {
+			Instantiate (HardAI);
+		}
+
+	}
 
 
 }
