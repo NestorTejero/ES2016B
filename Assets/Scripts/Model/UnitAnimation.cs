3a0164b7e53e97abbf43c034a4e767e28e2207b6
sashadgt <sashadgt@gmail.com>
2016-12-01 06:10:01 +0100

    Minor prefab fixes

---
+++ Assets/Scripts/Model/UnitAnimation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitAnimation : MonoBehaviour
+{
+
+    private Animator animController;
+    //We will use hash values for greatly increasing performance
+    private int walkHash;
+    private int dieHash;
+    private int attackHash;
+
+
+    void Start()
+    {
+
+        animController = GetComponent<Animator>();
+        walkHash = Animator.StringToHash("Walk");
+        attackHash = Animator.StringToHash("Attack");
+        dieHash = Animator.StringToHash("Die");
+    }
+
+    public void Attack()
+    {
+        animController.SetTrigger(attackHash);
+        Debug.Log("Animation Attack");
+    }
+
+    public void Die()
+    {
+        animController.SetTrigger(dieHash);
+        Debug.Log("Animation Die");
+
+    }
+    public void Walk()
+    {
+        animController.SetBool(walkHash, true);
+        Debug.Log("Animation Walk");
+    }
+}
