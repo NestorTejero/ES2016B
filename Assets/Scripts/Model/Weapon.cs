78f4557ba947911636cd7877289f76356ebc49a9
sashadgt <sashadgt@gmail.com>
2016-12-02 13:41:12 +0100

    Unit animation and scripts (#308)
    * Created Working animation Controllers
    
    * Created missing unit prefab
    
    * added missing controller
    
    * Minor prefab fixes
    
    * Animatios Almost working
    
    * more fixes + all units in shop, need fix direction of units
    
    * dead anim when dies
    
    * Animations seem to work
    
    * Created Die function for unit to clean code and set Navmeshdata from script
    
    * more fixes
    
    * code celanup
    
    * More and more fixes , with avnimations scritps and everything else
    
    * Aded texture parameters to Unit script and asigned texturees toprefab
    
    * testing texture and materials
    
    * Made prebafs have fucking texture and testing aplyying it
    
    * Minor fixes
    
    * UnitAnimation set animation speed added
    
    * Triger on Units prefabs on model disabled

--- Assets/Scripts/Model/Weapon.cs
+++ Assets/Scripts/Model/Weapon.cs
@@ -18,6 +18,7 @@ public class Weapon : MonoBehaviour
     private List<CanReceiveDamage> targets;
     public float upgradeFactor;
 
+    private UnitAnimation animScript;
     // Use this for initialization
     private void Start()
     {
@@ -46,6 +47,9 @@ public class Weapon : MonoBehaviour
         source_death = GameObject.Find("Death Audio Source").GetComponent<AudioSource>();
         source_shoot = GameObject.Find("Shoot Audio Source").GetComponent<AudioSource>();
         Debug.Log("WEAPON CREATED");
+
+
+        
     }
 
     // Upgrade weapon features
@@ -120,6 +124,11 @@ public class Weapon : MonoBehaviour
             proj_clone.GetComponent<Projectile>().Shoot(target, currentDamage);
             Destroy(proj_clone, 3.0f);
         }
+        //Animation data
+        if (tag == "Unit")
+        {
+            animScript.Attack();
+        }
     }
 
     public void setSourceDeath(AudioSource death)
@@ -130,5 +139,9 @@ public class Weapon : MonoBehaviour
     public void setSourceShoot(AudioSource shoot)
     {
         source_shoot = shoot;
+    }
+    public void setAnimScript(UnitAnimation ascript)
+    {
+        this.animScript = ascript;
     }
 }
