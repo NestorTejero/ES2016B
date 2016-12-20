886be130d3d8fb0043186e6e737c2136bbd6cdf5
niko227 <nk_bcn227@hotmail.com>
2016-12-21 00:34:51 +0100

    cambios prefabs

--- Assets/Scripts/Model/Players/AI.cs
+++ Assets/Scripts/Model/Players/AI.cs
@@ -10,6 +10,7 @@ public abstract class AI : Player
         unitsWave = 0;
         InvokeRepeating("Play", 15.0f, 2.0f);
         InvokeRepeating("AutoCoins", 0.0f, 1.0f);
+		InvokeRepeating ("ExtraCoins", 60.0f, 1.0f);
     }
 
     /* Give human player time to upgrade or repair */
@@ -29,4 +30,9 @@ public abstract class AI : Player
     {
         numCoins += 5;
     }
+
+	protected void ExtraCoins()
+	{
+		numCoins += 15;
+	}
 }
